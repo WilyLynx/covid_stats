4ae230f75f5e2289bb42bfe5f7a15e630255d3e5
Michal Mozdzonek <michaljmozdzonek@gmail.com>
2020-10-06 16:04:58 +0200

    Adjust annotation on plot

--- mcp/StatsPlotter.py
+++ mcp/StatsPlotter.py
@@ -17,21 +17,22 @@ class BasePlotter:
         last_y = self.stats['aktywni'][last_X]
         plt.plot([last_X], [last_y], 'bo')
         ax.set_title(f'Aktywne przypadki covid-19 w powiecie mińskim\n Dane z dnia {datetime.date.today()}')
+        plt.ylabel('Aktywni')
+        plt.xlabel('Data')
         ax.text(1, 0, 'Źródło danych: http://minsk.psse.waw.pl/ ', fontsize=12, color='black',
                 horizontalalignment='right',
                 verticalalignment='bottom',
                 transform=ax.transAxes)
 
-
         ax.set_xlim(self.stats.index.min(), self.stats.index.max() + pd.DateOffset(days=4))
         ax.set_ylim(0, 1.1 * self.stats['aktywni'].max())
         plt.subplots_adjust(left=0.1, bottom=0.25, right=0.9, top=0.9)
 
-        label = "{:.2f}".format(last_y)
+        label = "{:.0f}".format(last_y)
         plt.annotate(label,
                      (last_X, last_y),
                      textcoords="offset points",
-                     xytext=(0, 10),
+                     xytext=(5, 10),
                      ha='center')
 
         ax.grid(True)
