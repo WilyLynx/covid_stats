c254bf2296837eff826f7af01998870bc226c8b1
Michal Mozdzonek <michaljmozdzonek@gmail.com>
2020-10-12 15:11:20 +0200

    Adjust date formats on plot

--- mcp/StatsPlotter.py
+++ mcp/StatsPlotter.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
 import datetime
+import matplotlib.dates as mdates
 import pandas as pd
 
 
@@ -13,16 +14,23 @@ class BasePlotter:
         sns.set_style("whitegrid")
         fig, ax = plt.subplots()
         ax = sns.lineplot(x=self.stats.index, y=self.stats["aktywni"], ax=ax)
+
         last_X = self.stats.index.max()
         last_y = self.stats['aktywni'][last_X]
         plt.plot([last_X], [last_y], 'bo')
-        ax.set_title(f'Aktywne przypadki covid-19 w powiecie mińskim\n Dane z dnia {datetime.date.today()}')
-        plt.ylabel('Aktywni')
-        plt.xlabel('Data')
+
+        ax.set_title(f'Aktywne przypadki covid-19 w powiecie mińskim\n Dane z dnia {last_X.date().strftime("%d.%m.%Y")}')
         ax.text(1, 0, 'Źródło danych: http://minsk.psse.waw.pl/ ', fontsize=12, color='black',
                 horizontalalignment='right',
                 verticalalignment='bottom',
                 transform=ax.transAxes)
+
+        plt.ylabel('Aktywni')
+        plt.xlabel('Data')
+        locator = mdates.AutoDateLocator(minticks=9, maxticks=15)
+        formatter = mdates.ConciseDateFormatter(locator)
+        ax.xaxis.set_major_locator(locator)
+        ax.xaxis.set_major_formatter(formatter)
 
         ax.set_xlim(self.stats.index.min(), self.stats.index.max() + pd.DateOffset(days=4))
         ax.set_ylim(0, 1.1 * self.stats['aktywni'].max())
@@ -36,4 +44,3 @@ class BasePlotter:
                      ha='center')
 
         ax.grid(True)
-        fig.autofmt_xdate()
--- minsk_hist.py
+++ minsk_hist.py
@@ -4,17 +4,22 @@ import os
 import matplotlib.pyplot as plt
 import pandas as pd
 
-from mcp.CovidParsers import MinskPSSEParser
-from mcp.StatsPlotter import BasePlotter
-from mcp.CovidDb import CovidDb
+from mcp import CovidParsers as cp
+from mcp import StatsPlotter as sp
+from mcp import CovidDb as cdb
+from mcp import TextInfoWriter as tiw
+from mcp import MinskDistrictInfo as minfo
+import locale
 
 MINISK_CSV = 'data/minsk.csv'
 DB_PATH = 'data/covid.db'
+FB_PATH = 'fb'
 IMAGES_FOLDER = 'images'
 
+locale.setlocale(locale.LC_ALL, 'pl_PL')
 
 download_new_data = False
-db_ctx = CovidDb(DB_PATH)
+db_ctx = cdb.CovidDb(DB_PATH)
 if db_ctx.table_exists():
     stats = db_ctx.getStats()
     if stats.index.max() < datetime.date.today():
@@ -25,15 +30,19 @@ else:
 
 if download_new_data:
     print("FETCHING NEW DATA")
-    parser = MinskPSSEParser()
+    parser = cp.MinskPSSEParser()
     stats = parser.fetch_data()
     stats['aktywni'] = stats['potwierdzeni'] - stats['ozdrowieńcy'] - stats['zmarli']
     db_ctx.insertStats(stats)
 
 db_ctx.close()
-plotter = BasePlotter(stats.last('3M'))
 
+plotter = sp.BasePlotter(stats.last('3M'))
 plotter.plot_active()
+
+msg = tiw.TextInfoWriter(stats, minfo.MinskDistrictInfo()).get_facebook_msg()
+with open(os.path.join(FB_PATH, 'info.txt'), 'w') as f:
+    f.write(msg)
 
 plt.savefig(os.path.join(IMAGES_FOLDER, 'active.png'))
 plt.savefig(os.path.join(IMAGES_FOLDER, 'active.svg'))
