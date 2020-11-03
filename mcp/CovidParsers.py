1179b16080fa0df18570f32af4f76d03f1610ccc
Michal Mozdzonek <michaljmozdzonek@gmail.com>
2020-11-03 20:16:33 +0100

    Little refactor

--- mcp/CovidParsers.py
+++ mcp/CovidParsers.py
@@ -11,15 +11,14 @@ class MinskPSSEParser:
         self._fetch_url = url
 
     def fetch_data(self) -> DataFrame:
-        messages = self._get_messages()
-        covid_stats = self._extract_stats(messages)
-        covid_stats = self._build_data_frame(covid_stats)
-        return covid_stats
+        stats = list(map(self._extract_stats, self._get_messages()))
+        return self._build_data_frame(stats)
 
     @staticmethod
     def _build_data_frame(covid_stats):
         covid_stats = pd.DataFrame(covid_stats,
-                                   columns=['data', 'kwarantanna', 'hospitalizowani', 'potwierdzeni', 'ozdrowieńcy', 'zmarli'])
+                                   columns=['data', 'kwarantanna', 'hospitalizowani', 'potwierdzeni', 'ozdrowieńcy',
+                                            'zmarli'])
         covid_stats.loc[:, 'data'] = pd.to_datetime(covid_stats['data'], format='%d.%m.%Y')
         covid_stats: DataFrame = covid_stats.set_index('data').sort_index(ascending=True)
         return covid_stats
@@ -35,52 +34,48 @@ class MinskPSSEParser:
         return messages
 
     @staticmethod
-    def _extract_stats(messages):
-        data = []
-        for msg in messages:
+    def _extract_stats(msg):
+        try:
+            msg_data = []
+            # date
+            if len(msg.find_all('h3')) > 0:
+                header = msg.find_all('h3')[-1]
+            else:
+                header = msg.find_all('em')[-1]
+            date = re.findall('\d{2}\.\d{2}\.\d{4}', str(header))[0]
+            msg_data.append(date)
+
+            # features
+            rows = msg.find('ul').find_all('li')
             try:
-                msg_data = []
-                # date
-                if len(msg.find_all('h3')) > 0:
-                    header = msg.find_all('h3')[0]
-                else:
-                    header = msg.find_all('em')[0]
-                match = re.search('\d{2}\.\d{2}\.\d{4}', str(header))
-                date = match.group(0)
-                msg_data.append(date)
-
-                # features
-                rows = msg.find('ul').find_all('li')
-                try:
-                    msg_data.append(int([r for r in rows if 'kwarantanną' in str(r)][0].contents[3].contents[0]))
-                except Exception:
-                    msg_data.append(None)
-
-                try:
-                    msg_data.append(int([r for r in rows if 'hospitalizowanych ' in str(r)][0].contents[3].contents[0]))
-                except Exception:
-                    msg_data.append(None)
-
-                try:
-                    msg_data.append(int([r for r in rows if 'potwierdzonych' in str(r)][0].contents[3].contents[0]))
-                except Exception:
-                    msg_data.append(None)
-
-                try:
-                    row = str([r for r in rows if 'ozdrowieńców' in str(r)][0])
-                    num = re.search('[0-9]+', row)[0]
-                    msg_data.append(int(num))
-                except Exception:
-                    msg_data.append(None)
-
-                try:
-                    row = str([r for r in rows if 'zgonów' in str(r)][0])
-                    num = re.findall('[0-9]+', row)[1]
-                    msg_data.append(int(num))
-                except Exception:
-                    msg_data.append(None)
-
-                data.append(msg_data)
+                msg_data.append(int([r for r in rows if 'kwarantanną' in str(r)][0].contents[3].contents[0]))
             except Exception:
-                pass
-        return data
+                msg_data.append(None)
+
+            try:
+                msg_data.append(int([r for r in rows if 'hospitalizowanych ' in str(r)][0].contents[3].contents[0]))
+            except Exception:
+                msg_data.append(None)
+
+            try:
+                msg_data.append(int([r for r in rows if 'potwierdzonych' in str(r)][0].contents[3].contents[0]))
+            except Exception:
+                msg_data.append(None)
+
+            try:
+                row = str([r for r in rows if 'ozdrowieńców' in str(r)][0])
+                num = re.search('[0-9]+', row)[0]
+                msg_data.append(int(num))
+            except Exception:
+                msg_data.append(None)
+
+            try:
+                row = str([r for r in rows if 'zgonów' in str(r)][0])
+                num = re.findall('[0-9]+', row)[1]
+                msg_data.append(int(num))
+            except Exception:
+                msg_data.append(None)
+
+            return msg_data
+        except Exception:
+            pass
